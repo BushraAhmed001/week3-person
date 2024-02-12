bdc3014e07e7b1a8dc35e4e081616fbff09a7d41
Bushra Ahmed <bushraa.ahmad93@gmail.com>
2024-02-12 11:14:09 +0000

    person

---
+++ main.py
@@ -0,0 +1,8 @@
+from person import Person
+liam = Person("Liam", "Tall", 30)
+bushra = Person("Bushra", "Short", 30)
+print(liam.height)
+print(bushra.name)
+print(bushra.height)
+print(bushra.age)
+print(f"My name is {bushra.name} and I am {bushra.age} years old.")
---
+++ person.py
@@ -0,0 +1,6 @@
+class Person():
+     def __init__(self, person_name, person_height, person_age):
+         self.name = person_name
+         self.height =  person_height
+         self.age =person_age
+         
